c61e8df82eae63143c45c3d0655de703200dcafb
ka2er <personseb+git@gmail.com>
2018-04-19 16:26:27 +0200

    [NEW] 1st commit a o365 enpoints extrator ng (consume REST API)

---
+++ getO365Endpoints-ng.sh
@@ -0,0 +1,199 @@
+#!/usr/local/bin/bash
+## ^^ v4 ...
+
+restO365Endpoints="https://endpoints.office.com/endpoints/o365worldwide?clientrequestid=09a38806-ef8b-454a-be25-7e01f129b4a2&TenantName=lfdj"
+
+show_requirements() {
+  echo "$1 is missing."
+  echo 'Please install it with `brew install '.$1.'`' >&2
+}
+
+show_help() {
+  echo "Show O365 endpoints description in unix parsable format.
+Load source data from here : $restO365Endpoints
+
+Doc is here : https://support.office.com/en-us/article/Managing-Office-365-endpoints-99cab9d4-ef59-4207-9f2b-3728eb46bf9a#ID0EACAAA=4._Web_service
+Category are here : https://blogs.technet.microsoft.com/onthewire/2018/04/06/new-office-365-url-categories-to-help-you-optimize-the-traffic-which-really-matters/
+
+- Optimize for a small number of endpoints that require low latency unimpeded connectivity which should bypass proxy servers, network SSL break and inspect devices, and network hairpins.
+- Allow for a larger number of endpoints that benefit from low latency unimpeded connectivity. Although not expected to cause failures, we also recommend bypassing proxy servers, network SSL break and inspect devices, and network hairpins. Good connectivity to these endpoints is required for Office 365 to operate normally.
+- Default for other Office 365 endpoints which can be directed to the default internet egress location for the company WAN.
+ 
+
+Sample output (product ip_range ports_range category):
+
+  o365 23.100.86.91/32 80 optimize
+  o365 23.101.14.229/32 80,443 allow
+  o365 23.101.30.126/32 100-200 default
+Todo:
+
+ - arg : category (1-N)
+ - arg : product (1-N)
+ - arg : type (ip/dns)
+
+Usage:
+
+    $0 [-h] [-v]
+
+  Options:
+
+    -v
+      Verbose output.
+
+    -h
+      Show this help.
+" >&2
+}
+
+## arguments parsing #########
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+verbose=0
+
+while getopts "h?v" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    v)  verbose=1
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+#######################################
+
+wget="$(which wget)"
+jq="$(which jq)"
+
+[[ $wget == "" ]] && show_requirements 'wget' && exit 1
+[[ $jq == "" ]] && show_requirements 'jq' && exit 1
+
+json_cache="/tmp/o365endpoints-ng-`date "+%Y%m%d"`.txt"
+
+
+#if [[ -f $json_cache ]] 
+#then
+#	cat $json_cache
+#	exit 0	
+#fi
+
+services=[]
+product=""
+address=""
+ports=""
+category="allow"
+
+
+### retrieve REST source
+json=$($wget --timeout 3 -t1 -q -O- $restO365Endpoints)
+wget_error=$?
+[[ $wget_error != 0 ]] && echo "Error while retrieving endpoints (wget err_code=$wget_error)" && exit 1
+
+## dev only : cache fetch result
+echo $json > $json_cache
+
+
+### parse REST
+
+# expr to list services
+expr_services="unique_by(.serviceArea)|.[].serviceArea"
+# expr to list keys word specific to a category level
+expr_json_cat_keys="[.[] | keys | .[]] | unique | map(select(. | startswith(\"$category\"))) | join(\" \")"
+# "optimizeTcpPorts",
+# "optimizeUdpPorts",
+# "optimizeUrls"
+
+services=`echo $json | jq -r "$expr_services"`
+category_keys=`echo $json | jq -r "$expr_json_cat_keys"`
+
+## build select categories filter
+expr_json_cat_select_filter=""
+expr_json_cat_bash_out=""
+for category_key in $category_keys; do
+  expr_json_cat_select_filter+=" select(.$category_key) ,"
+done;
+
+# remove last comma
+expr_json_cat_select_filter=${expr_json_cat_select_filter%?}
+
+for service in $services; do
+
+  ## TODO generate keyword optimize, default or allow at run time
+
+  ### la on a pour un service(ex Skype) les regles de la categorie(optimize)
+  #service_ressources=`echo $json | jq -r "[.[] | select(.optimizeTcpPorts), select(.optimizeUdpPorts), select(.optimizeUrls) | select(.serviceArea==\"$service\") ] | unique"`
+  service_ressources=`echo $json | jq -r "[.[] | $expr_json_cat_select_filter | select(.serviceArea==\"$service\") ] | unique"`
+
+    # split as bash arrays
+    nb_ressources=`echo $service_ressources | jq -r length`
+    # for each bash array, retrieve key
+    while [ $nb_ressources -gt 0 ] ; do
+      ((nb_ressources--))
+
+      ressource=`echo $service_ressources | jq -r .[$nb_ressources]`
+
+      echo "-------------------"
+      echo $ressource
+
+#      rule=`echo $ressource | jq -r "$expr_json_cat_bash_out"`
+
+      # check if ips rules or domains rules
+      has_ip=`echo $ressource | jq -r "has(\"ips\")"`   
+
+      ## dump nets (url or ips)
+
+      ## dump ports (tcp+udp)
+
+      ## harcode cat
+
+      # maps json to shell variable
+      if $has_ip; then
+        ## add ips and filter IPv6
+        #rule+=`echo $ressource | jq -r "\" ips=\"+([.ips | .[] | select(test(\"::\")==false)]| join(\",\"))"`
+        expr_json_cat_net='[.ips | .[] | select(test("::")==false)]'
+      else 
+       # $nets = `echo $service_ressources | jq -r "[ $expr_json_cat_ips ]" `
+        url_key=$category"Urls"
+        expr_json_cat_net="select(.$url_key)|.$url_key"
+
+      fi;
+
+      ## collect net endpoints
+      nets=`echo $ressource | jq -r "$expr_json_cat_net" `
+      nets=`echo $nets | tr -d "\n"`
+
+      ## collect port endpoints
+      tport_key=$category"TcpPorts"
+      uport_key=$category"UdpPorts"
+
+      ### TODO !!! transform udp ports
+      ports=`echo $ressource | jq -r "[ .$tport_key? , .$uport_key+\"/udp\"| select(test(\"^/udp$\")==false) | strings ]"`
+      ports=`echo $ports | tr -d "\n"`
+
+      ## summup in online
+      echo "[[\"$service\"], $nets, $ports , [\"$category\"]]" | jq -r 'combinations | join(" ")'
+    
+   ###   echo "$product $address" >> $xml_cache
+
+    done;
+
+done;
+
+## TODO
+## 
+## out :
+##  - HIPS
+##  - proxy.pac
+##  - firewall
+##  - proxy (SSL OFF, WL auth, etc..)
+
+
+exit 0
